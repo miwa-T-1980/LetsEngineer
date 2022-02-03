ebfbf84c5d94b48c63a1b3c8a8092b36558b7b42
miwa-T-1980 <88562065+miwa-T-1980@users.noreply.github.com>
2022-02-03 15:42:28 +0900

    PHP2-11課題提出
    PHP2章「フォームに入力した内容を取得する」の課題を提出します

---
+++ 2-11/index.php
@@ -0,0 +1,22 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="e_mail" >
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<?php
+echo "仕様書とは「どこにどのような機能を持たせるのか」「どこからどのように遷移させるのか」といったプロダクトのあるべき姿を記載したもの。";
+echo "<br>";
+echo "設計書とは、完成するまでの制作工程を明確にした資料。制作過程を示すもの。";
+echo "<br>";
+echo "<br>";
+echo "Gitは、プログラムのソースコードなどの変更履歴を記録・追跡するための分散型バージョン管理システムのこと。";
+echo "<br>";
+echo "<br>";
+echo "マージツールは，FDLファイル又はDTDファイルの変更内容をMDLファイルにマージできます。";
+echo "<br>";
+?>
---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$e_mail = $_POST['e_mail'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $e_mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
