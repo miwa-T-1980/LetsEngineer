b6a41a7aa4b181998d3ca95c81d9098d2eb271d3
miwa-T-1980 <88562065+miwa-T-1980@users.noreply.github.com>
2022-02-18 11:43:03 +0900

    2-12課題再提出の件
    PHP2章2-12の課題を再提出いたします

--- 2-12/index.php
+++ 2-12/index.php
@@ -8,16 +8,11 @@
         <br>
         個数：
         <select name="items">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+            <?php for ($i=1;$i<=10;$i++){ ?>
+                <option value="<?php echo $i; ?>">
+                    <?php echo $i; ?>
+                </option>
+            <?php } ?>
         </select>
         <br>
         <input type="submit" value="申込" /><br>
